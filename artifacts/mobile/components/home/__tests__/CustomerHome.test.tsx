7e600a225c41c3788b9b2bc39fed816dfd02c47e
Pacifique Harerimana <harerimanapacifique95@gmail.com>
2026-06-23 23:55:32 +0200

    Fix typecheck errors by declaring node __dirname and typing share route return value

--- artifacts/mobile/components/home/__tests__/CustomerHome.test.tsx
+++ artifacts/mobile/components/home/__tests__/CustomerHome.test.tsx
@@ -2,6 +2,8 @@ import { fireEvent, render, screen } from '@testing-library/react-native';
 import React from 'react';
 import CustomerHome from '../CustomerHome';
 
+declare const __dirname: string;
+
 const mockPush = jest.fn();
 const mockSetParams = jest.fn();
 
